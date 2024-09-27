542210ce12ff8b792fd6c43b41c9f48c8fa6a9ff
Estela de Vega <130656156+estelaV9@users.noreply.github.com>
2024-09-27 16:52:48 +0200

    terminar agenda

--- Tema3_Python/Python_SGE/02estructuras_control/agenda_contactos.py
+++ Tema3_Python/Python_SGE/02estructuras_control/agenda_contactos.py
@@ -12,29 +12,47 @@ while(opcion != 0):
     opcion = int(input("Introduzca una opcion: "))
     match opcion:
         case 1:
-            print("********* BUSCAR CONTACTO *********")
+            print("\n********* BUSCAR CONTACTO *********")
             name_contact_search = input("¿Qué nombre de contacto desea buscar? ")
-
+            if name_contact_search in my_contact_list: # IF IN
+                input(f"El usuario {name_contact_search} esta registrado en la agenda")
+            else:
+                input(f"El usuario {name_contact_search} NO esta registrado")
 
         case 2:
-            print("********* INSERTAR CONTACTO *********")
+            print("\n********* INSERTAR CONTACTO *********")
             name_contact = input("Introduzca el nombre del contacto: ")
             phone_contact = input("Introduzca el numero de telefono del contacto (máximo 9 dígitos): ")
             # COMPRUEBA SI SOLO TIENE NUMEROS Y UN MAX DE 9 DIGITO
             # isdigit(): COMPRUEBA SI TODOS LOS CARACTERES EN LA CADENA SON NUMEROS
-            if telefono.isdigit() and len(telefono) <= 9:
+            if phone_contact.isdigit() and len(phone_contact) < 9:
                 print("Error: Debes introducir un número válido con máximo 9 dígitos. Intenta de nuevo.")
-            my_contact_list.append(0)
+            my_contact_list.append(name_contact) # DENTRO DEL APPEND SE PONE EL PARAMETRO QUE QUEREMOS GUARDAR
+        
         case 3:
-            print("********* ACTUALIZAR CONTACTO *********")
-            
+            print("\n********* ACTUALIZAR CONTACTO *********")
+            name_contact = input("Introduza el nombre del contacto a actualizar: ")
+            name_update = input("Introduzca el nuevo nombre del contacto: ")
+            # ENCONTRAMOS EL INDICE DEL CONTACTO Y ACTUALIZAMOS
+            index = my_contact_list.index(name_contact)
+            my_contact_list[index] = name_update
+            print(f"El contacto {name_contact} ha sido actualizado a {name_update}.")
            
         case 4:
-            print("********* ELIMINAR CONTACTO *********")
-          
+            print("\n********* ELIMINAR CONTACTO *********")
+            name_contact = input("Introduzca el nombre del contacto a eliminar: ")
+            if name_contact in my_contact_list: # IF IN
+                my_contact_list.remove(name_contact)
+                input(f"El usuario {name_contact} ha sido eliminado con exito")
+            else:
+                input(f"El usuario {name_contact_search} NO esta registrado")
+
         case 5:
-            print("********* MOSTRAR CONTACTOS *********")
-          
+            print("\n********* MOSTRAR CONTACTOS *********")
+            for i in range(0,len(my_contact_list)):
+                print(my_contact_list[i])
+
         case _:
             print("numero no valido")
-          
+
+print("¡Hasta pronto!")
