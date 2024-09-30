1b5fc9658c294f40445a64ff0dae620ffbb7022d
estelaV9 <esteladeveg@gmail.com>
2024-09-30 13:47:32 +0200

    Crear proyecto sets y añadirle los datos

---
+++ Tema3_Python/Python_SGE/04estructuras_datos/sets.py
@@ -0,0 +1,18 @@
+# *************************** SETS ***********************
+# SE USAN PARA EVITAR DUPLICADOS PORQUE TIENE UN ACCESO MUY APIDO. EL AL LISTA ES MAS LENTO
+my_set = {"Ana", "Pedro"}
+print(my_set)
+
+# INSERTAR ELEMENTO
+my_set.add("Ribera")
+print(my_set)
+# set[0] NO FUNCIONA
+
+# ELIMINAR ELEMENTO
+my_set.remove("Ana")
+print(my_set)
+
+# ACTUALIZAR: UPDATE AMPLIA LOS DATOS QUE HAY EN EL SET (cancatena)
+# PARA CAMBIAR UN DATO POR OTRO ES NECESARIO BORRARLO Y CREARLO DE NUEVO
+
+# ORDENAR: POR DEFINICION, UN SET NO PUEDE SER ORDENADO
