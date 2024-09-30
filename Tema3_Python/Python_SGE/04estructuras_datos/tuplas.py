51f266edbb835ce665361a8da3e40c14f49a510b
estelaV9 <esteladeveg@gmail.com>
2024-09-30 13:25:13 +0200

    Crear proyecto tuplas

---
+++ Tema3_Python/Python_SGE/04estructuras_datos/tuplas.py
@@ -0,0 +1,21 @@
+# ****************** TUPLAS *********************
+""" INVESTIGAR LA ESTRUCTURA DE DATOS TUPLAS 
+Las tuplas son un tipo o estructura de datos que permite almacenar datos de una manera 
+muy parecida a la lista, pero son INMUTABLES, es decir, que no pueden ser modificadas
+una vez declaradas y se inicializan con (). """
+
+
+# INSERCION
+tupla = (1, 2, 3)
+print(tupla) # (1, 2, 3)
+
+tupla = 1, 2, 3 # se puede declarar sin ()
+print(tupla)
+
+# BORRADO
+
+
+# ACTUALIZACION
+
+
+# ORDENACION
