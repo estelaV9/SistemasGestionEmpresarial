b7cd85d9d88b4d23a1a17233e4786e1aef43cb4a
estelaV9 <esteladeveg@gmail.com>
2024-10-07 13:15:30 +0200

    crear el ejercicio json e incluir comentario con la informacion sobre los ficheros json

---
+++ Tema3_Python/Python_SGE/08json/ejercicio_json.py
@@ -0,0 +1,20 @@
+""" EJERCICIO 8: FICHEROS JSON 
+JSON (JavaScript Object Notation) es un formato de intercambio de datos ligero que se utiliza
+comúnmente para representar datos estructurados. Se basa en la notación de objetos de JavaScript.
+
+Elementos clave de JSON:
+ - Objetos: Representados por llaves {}, contienen pares de clave-valor. {"nombre": "Juan", "edad": 30}
+ - Array: Representados por corchetes [], son listas ordenadas de valores. ["rojo", "verde", "azul"]
+ - Tipos de datos: Puede contener cadenas (string), números (number), booleanos (true/false), nulos (null), objetos y arrays.
+ - Claves: Siempre deben ser cadenas y deben estar entre comillas dobles.
+
+Características de JSON:
+ - De fácil lectura.
+ - Soporta estructuras anidadas (objetos dentro de objetos, array dentro de objetos, etc.).
+ - Suele ser aplicado más comúnmente en un entorno web, donde recibir datos entre cliente y servidor.
+
+Usos principales de JSON:
+ - Intercambio de datos entre aplicaciones y servicios web (API).
+ - Almacenamiento de configuración y datos en aplicaciones.
+ - Intercambio de datos entre lenguajes de programación diferentes. """
+ 
