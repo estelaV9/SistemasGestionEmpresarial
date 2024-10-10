0cc57bf71646a59e550a25ae03ded8e30807dcc1
estelaV9 <esteladeveg@gmail.com>
2024-10-10 12:25:17 +0200

    intentar corregir el ejercicio del jsonClase y crear el ejercicio de http

--- Tema3_Python/Python_SGE/08json/ejercicio_json_clase.py
+++ Tema3_Python/Python_SGE/08json/ejercicio_json_clase.py
@@ -2,20 +2,20 @@ import os
 import json
 
 # CREAR UNA FUNCION QUE GENERE UN FICHERO JSON CON LOS MISMOS DATOS DEL EJERCICIO ANTERIOR
+
+data = {
+    "nombre": "Eduardo",
+    "edad": 23,
+    "fecha_nacimiento": "12-02-2004",
+    "modulos": ["Java", "Kotlin", "Python"]
+}
+
 json_file = "prueba.json"
 def generar_fichero ():
-    data = {
-     "nombre": "Eduardo",
-     "edad": 23,
-     "fecha_nacimiento": "12-02-2004",
-     "modulos": ["Java", "Kotlin", "Python"]
-    }
-
     with open(json_file, "w") as json_data:
         json_file.dump(data, json_data) # dump CONVIERTE EL DICCIONARIO EN FORMATO JSON Y LO ESCRIBE 
         
-    with open(json_file, "r") as json_data:
-        print(json_data.read())
+generar_fichero()
 
 
 # CREAR UNA CLASE PERSON QUE TENGA UN CONSTRUCTOR QUE RECIBA COMO ATRIBUTOS LOS DATOS DEL FICHERO
@@ -29,8 +29,13 @@ class Person:
 
 
 with open(json_file, "r") as json_data:     
+    json_file = json.load(json_data) # CONVIRTE LOS DATOS DEL FICHERO JSON EN UN DICCIONARIO
+    Person = Data(
+        
+        
+    )
     person = Person(json_file.__getattribute__)
-    
+    print
 # ELIMINAR EL FICHERO JSON
 print("Borrando el fichero json")
 os.remove(json_file)
---
+++ Tema3_Python/Python_SGE/09http/ejercicio_http.py
@@ -0,0 +1,42 @@
+# ************************** EJERCICIO 9 *********************
+
+""" Servicio web: Un servicio web es un sistema diseñado para soportar la interoperabilidad entre aplicaciones
+a través de redes. Permite a diferentes aplicaciones comunicarse y compartir datos.
+
+RESTful API: Es un tipo de API (Interfaz de Programación de Aplicaciones) que sigue los principios REST
+(Transferencia de Estado Representacional). Utiliza métodos HTTP y es sin estado, lo que significa que cada
+solicitud del cliente al servidor debe contener toda la información necesaria para entender y procesar
+la solicitud.
+
+Protocolo HTTP: El Protocolo de Transferencia de Hipertexto (HTTP) es un protocolo de comunicación
+que se utiliza para la transmisión de información en la web. Es un protocolo sin estado que permite
+la transferencia de datos entre un cliente (como un navegador web) y un servidor.
+
+Peticiones HTTP: Son solicitudes que un cliente envía a un servidor para obtener información o
+realizar alguna acción. Los métodos más comunes son GET, POST, PUT y DELETE.
+
+Respuestas HTTP: Son los mensajes que un servidor envía de vuelta a un cliente después de procesar
+una solicitud. Algunos códigos de respuesta comunes son:
+- 200: OK. La solicitud fue exitosa y el servidor devolvió la información solicitada.
+- 404: Not Found. El recurso solicitado no fue encontrado en el servidor.
+- 500: Internal Server Error. Se produjo un error en el servidor al procesar la solicitud.
+
+La librería de Python para realizar peticiones HTTP es `requests`. Para ello instalarlo 'pip install requests' """
+
+import requests
+
+# REALIZAR UNA PETICIÓN GET PARA OBTENER UNA PAGINA WEB
+url = "https://github.com/estelaV9"  # IR A MI GITHUB
+
+try:
+    x = requests.get(url)
+    
+    # SE VERIFICA QUE LA PETICION FUESE EXITOSA
+    if x.status_code == 200:
+        print("La petición fue exitosa. Contenido de la web:")
+        print(x.text)  # MOSTRAR EL CONTENIDO DE LA RESPUES
+    else:
+        print(f"La petición no tuvo éxito. Código de error: {x.status_code}")
+
+except Exception as e:
+    print(f"Ocurrió un error al realizar la petición: {e}")
