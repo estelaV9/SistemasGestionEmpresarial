4776efabfce93945aa90b4da9e739ea362dcd97f
estelaV9 <esteladeveg@gmail.com>
2024-10-14 14:02:28 +0200

    modificar menu

--- Tema3_Python/Python_SGE/12proyecto_cubex/main.py
+++ Tema3_Python/Python_SGE/12proyecto_cubex/main.py
@@ -3,17 +3,19 @@ REALIZADO POR: Estela de Vega Martin 2DAM
 CURSO: 24/25
 ASIGNATURA: Sistemas de Gestión Empresarial """
 
-print("¡Bienvenido a CubeX! ¿Qué desea hacer en nuestra tienda?")
 try:
-    print("**** MENU ****",
+    opcion = -1
+    print("¡Bienvenido a CubeX! ¿Qué desea hacer en nuestra tienda?")
+    while (opcion != 0):
+        print("**** MENU ****",
           "\n1 - Ver todos los productos disponibles.",
           "\n2 - Ver tus productos.",
           "\n3 - Crear productos",
           "\n4 - Modificar productos",
           "\n5 - Eliminar productos",
-          "\n6 - Salir de la tienda.")
-    opcion = int(input("Tu ooción: "))
-    while (opcion != 6):
+          "\n0 - Salir de la tienda.")
+        opcion = int(input("Introduzca la opción: "))
+
         match opcion:
             case 1:
                 print("********** PRODUCTOS EN TIENDA **********")
@@ -25,8 +27,10 @@ try:
                 print("********** MODIFICAR PRODUCTOS **********")
             case 5:
                 print("********** ELIMINAR PRODUCTOS **********")
-            case _: 
-                print("Opción no válida. Introduzca un número del 1 al 6.")
+            case 0:
+                print("Gracias por visitar nuetra tienda.")
+            case _:
+                print("Opción no válida.")
         
     print(opcion)
 except Exception as e:
