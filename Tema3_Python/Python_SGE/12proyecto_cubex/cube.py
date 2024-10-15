0e6485fe1e367e37fafd7803cb850bb852fb1947
estelaV9 <esteladeveg@gmail.com>
2024-10-15 16:18:37 +0200

    eliminar la clase cubo y añadirle atributos a la clase producto

--- Tema3_Python/Python_SGE/12proyecto_cubex/cube.py
+++
@@ -1,6 +0,0 @@
-class cube:
-    def __init__(self, name, tipo, price, stock):
-        self.name = name
-        self.tipo = tipo
-        self.price = price
-        self.stock = stock
--- Tema3_Python/Python_SGE/12proyecto_cubex/product.py
+++ Tema3_Python/Python_SGE/12proyecto_cubex/product.py
@@ -1,3 +1,6 @@
 class product:
-    def __init__(self, productos):
-        self.productos = [] ""
+    def __init__(self, nombre, precio, stock, categoria):
+        self.nombre = nombre
+        self.precio = precio
+        self.stock = stock
+        self.categoria = categoria
