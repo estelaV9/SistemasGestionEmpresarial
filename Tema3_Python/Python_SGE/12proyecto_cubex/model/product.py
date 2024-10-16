510d2772812abd366bd485e6c3fa081269c97063
estelaV9 <esteladeveg@gmail.com>
2024-10-16 18:58:17 +0200

    añadir usuarios y crear un json para productos y añadir productos predeterminados

--- Tema3_Python/Python_SGE/12proyecto_cubex/model/product.py
+++ Tema3_Python/Python_SGE/12proyecto_cubex/model/product.py
@@ -1,9 +1,20 @@
 class product:
-    def __init__(self, nombre, precio, stock, categoria):
+    def __init__(self, nombre, precio, stock, categoria, owner):
         self.nombre = nombre
         self.precio = precio
         self.stock = stock
         self.categoria = categoria
+        self.owner = owner
+
+    def to_dict(self):
+        # CONVERTIR UN OBJETO A UN DICCIONARIO
+        return {
+            "nombre": self.nombre,
+            "precio": self.precio,
+            "stock": self.stock,
+            "categoria": self.categoria,
+            "owner": self.owner
+        }
 
     # METODO PARA DEVOLVER LOS PRODUCTOS EN TIENDA, SE LE PASA UNA LISTA DE PRODUCTOS Y SE RECORRE
     def devolver_productos (lista):
@@ -14,4 +25,4 @@ class product:
     def __str__(self):
         # METODO PARA DEVOLVER INFORMACION SOBRE LOS PRODUCTOS
         return (f"Producto: {self.nombre}, Precio: {self.precio}€, "
-                f"Stock: {self.stock} und, Categoria: {self.categoria} ")
+                f"Stock: {self.stock} und, Categoria: {self.categoria}, Propietario: {self.owner}")
