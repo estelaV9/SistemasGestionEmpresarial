87672ac0e968d9439a3cea1d7d533f208e2da9fa
estelaV9 <esteladeveg@gmail.com>
2024-10-16 18:38:42 +0200

    modificar el inicio de sesion en el main (controlar que ponga solo 1 o 2 y que si algo falla en la creacion/iniciar vuelva a saltar el incio de sesion y no el menu)

--- Tema3_Python/Python_SGE/12proyecto_cubex/main.py
+++ Tema3_Python/Python_SGE/12proyecto_cubex/main.py
@@ -20,31 +20,38 @@ try:
     cube7 = product("X-Man Bell Magnetic Skewb", 17.99, 6, "Skewb")
     list_cube = [cube1, cube2, cube3, cube4, cube5, cube6, cube7] # CREAR UNA LISTA CON ESOS PRODUCTOS
 
+    while True:
+        print("¿Cómo quieres entrar a nuestra tienda?\n1-Iniciando sesión\n2-Creando una cuenta")
+        metodo_login = int(input())
+        # CONTROLAR QUE PONGA LA OPCION 1 O 2
+        while metodo_login > 2 or metodo_login < 1:
+            metodo_login = int(input("Introduzca una opcion entre 1 y 2\n"))
 
-    print("¿Cómo quieres entrar a nuestra tienda?\n1-Iniciando sesión\n2-Creando una cuenta")
-    metodo_login = int(input())
-    if metodo_login == 1:
-        print("******** INICIAR SESION ********")
-        email = input("Email: ")
-        password = input("Contraseña: ")
-        user_log = user(email, password, name_user=None)
-        """ nota (para mi): para indicar que la funcion devuelva false en vez de poner '!' en Python es con 'not' """
-        if not user_dao.log_in(user_log):
-            print("No se ha encontrado usuario. Por favor, pruebe con otro usuario o crease una cuenta.")
-        else:
-            print("Inicio de sesión exitosa")
+        if metodo_login == 1:
+            print("******** INICIAR SESION ********")
+            email = input("Email: ")
+            password = input("Contraseña: ")
+            user_log = user(email, password, name_user=None)
+            """ nota (para mi): para indicar que la funcion devuelva false en vez de poner '!' en Python es con 'not' """
+            if not user_dao.log_in(user_log):
+                print("No se ha encontrado usuario. Por favor, pruebe con otro usuario o crease una cuenta.")
+            else:
+                print("Inicio de sesión exitosa")
+                break # SALE DEL BUCLE SI SE HA INICIADO SESION CORRECTAMENTE Y ENTRA EN LA TIENDA
 
-    else:
-        print("******** CREAR UNA CUENTA ********")
-        name_user = input("Nombre del usuario: ")
-        email = input("Email: ")
-        password = input("Contraseña: ")
-        new_user = user(email, password, name_user)
-        if user_dao.create_user(new_user):
-            print("Creación de cuenta exitosa")
-        else:
-            print("Ya tienes una cuenta, inicia sesión para entrar a nuestra tienda")
+        elif metodo_login == 2:
+            print("******** CREAR UNA CUENTA ********")
+            name_user = input("Nombre del usuario: ")
+            email = input("Email: ")
+            password = input("Contraseña: ")
+            new_user = user(email, password, name_user)
+            if user_dao.create_user(new_user):
+                print("Creación de cuenta exitosa")
+                break # SALE DEL BUCLE SI SE HA CREADO CUENTA CORRECTAMENTE Y ENTRA EN LA TIENDA
+            else:
+                print("Ya tienes una cuenta, inicia sesión para entrar a nuestra tienda")
 
+    # MENU PRINCIPAL DESPUES DEL INICIO DE SESION
     print("¡Bienvenido a CubeX! ¿Qué desea hacer en nuestra tienda?")
     while option != 0:
         print("**** MENU ****",
