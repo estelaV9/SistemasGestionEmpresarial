18d3fdef8c989c657df971549b83323b119dd792
estelaV9 <esteladeveg@gmail.com>
2025-01-09 13:15:22 +0100

    Añadir la clase User e indicarle en la peticion post

--- Tema10_ProyectoFinal/FastAPI/proyecto_curso_2425/main.py
+++ Tema10_ProyectoFinal/FastAPI/proyecto_curso_2425/main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
 
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+# DEFINIR pydantic PARA TRABAJAR CON LOS DATOS
+""" ********** USER MODEL *************** """
+class User(BaseModel):  # Schema
+    id: int
+    nombre: str
+    apellido: str
+    direccion: Optional[str]  # PARAMETRO OPCIONAL
+    telefono: int
+    creacion_user: datetime = datetime.now()  # FECHA POR DEFECTO
+
+
 # CREAMOS UNA INSTANCIA DE FASTAPI
 app = FastAPI()
 
@@ -9,10 +24,13 @@ app = FastAPI()
 def ruta1():
     return {"mensaje": "Hemos creado nuestra primera API!!!"}
 
-# PETICION POST
+
+# PETICION POST PARA INSERTAR USUARIO E IMPRIMIRLO POR CONSOLA
 @app.post("/ruta2")
-def ruta2(user):
-    print(user)
+# INDICAR QUE EL USUARIO QUE VA A RECIBIR SERA IGUAL AL MODELO QUE HEMOS CREADO
+def ruta2(user: User):
+    print(user) # IMPRIMIR EL USUARIO CUANDO SE EJECUTA
+    print(user.nombre) # IMPRIMIR EL NOMBRE
     return True
 
 
