1088f5c373e3009ca7e658e8e36587b2aa519bbc
estelaV9 <esteladeveg@gmail.com>
2025-01-16 13:16:57 +0100

    añadir los campos de username, password y correo al model user

--- Tema10_ProyectoFinal/FastAPI/proyecto_curso_2425/app/schemas.py
+++ Tema10_ProyectoFinal/FastAPI/proyecto_curso_2425/app/schemas.py
@@ -5,11 +5,13 @@ from datetime import datetime
 # DEFINIR pydantic PARA TRABAJAR CON LOS DATOS
 """ ********** USER MODEL *************** """
 class User(BaseModel):  # Schema
-    id: int
+    username: str
+    password: str
     nombre: str
     apellido: str
     direccion: Optional[str]  # PARAMETRO OPCIONAL
     telefono: int
+    correo: str
     creacion_user: datetime = datetime.now()  # FECHA POR DEFECTO
 
 
