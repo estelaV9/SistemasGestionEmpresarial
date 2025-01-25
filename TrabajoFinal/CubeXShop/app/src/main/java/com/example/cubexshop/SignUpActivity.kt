94243f8192aad1f0731a95ed9214e0711f8c8d0b
estelaV9 <esteladeveg@gmail.com>
2025-01-25 22:25:26 +0100

    agregar la funcionalidad de crear un usuario

--- TrabajoFinal/CubeXShop/app/src/main/java/com/example/cubexshop/SignUpActivity.kt
+++ TrabajoFinal/CubeXShop/app/src/main/java/com/example/cubexshop/SignUpActivity.kt
@@ -1,21 +1,32 @@
 package com.example.cubexshop
 
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.util.Patterns
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import com.example.cubexshop.dao.UserDAO
+import com.example.cubexshop.database.DatabaseHelper
 import com.example.cubexshop.databinding.ActivitySignupBinding
+import com.example.cubexshop.model.User
+import java.time.LocalDate
 
 class SignUpActivity : AppCompatActivity() {
     lateinit var miBinding: ActivitySignupBinding
+    private lateinit var userDAO: UserDAO
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
         miBinding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(miBinding.root)
+
+        userDAO = UserDAO(this) // SE INCIALIZA EL DAO
 
         miBinding.LoginTV.setOnClickListener {
             // CREA UN INTENT PARA ABRIR EL LoginActivity
@@ -44,6 +55,27 @@ class SignUpActivity : AppCompatActivity() {
 
             miBinding.messageErrorConfirmPassTV.text =
                 if (!confirmPasswordIsValid) validateConfirmPasswordMessage() else ""
+
+            if (validateName() && validatePassword() && validateConfirmPassword() && validateEmail()) {
+                val user = User(
+                    idUser = null,
+                    name = miBinding.nameET.text.toString(),
+                    email = miBinding.emailET.text.toString(),
+                    password = miBinding.passwordET.text.toString(),
+                    registrationDate = LocalDate.now()
+                ) // CREAR EL USUARIO
+
+                val userId = userDAO.addUser(user) // GUARDAMOS EN LA BASE DE DATOS
+
+                if (userId != -1L) {
+                    Toast.makeText(this, "Usuario creado con exito",
+                        Toast.LENGTH_SHORT).show()
+                    /**** ir a la pagina principal ***/
+                } else {
+                    Toast.makeText(this, "Error al crear el usuario",
+                        Toast.LENGTH_SHORT).show()
+                } // MOSTRAMOS UN TOAST DEPENDIENDO SI SE GUARDO BIEN
+            } // SI TODOS LOS CAMPOS SON CORRECTOS, LO GUARDAMOS EN LA BD
 
         } // CUANDO PULSE EL BOTON DE SIGNUP, SE VALIDARAN LOS CAMPOS
     }
